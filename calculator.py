56336a678250a66724396cdbedb65bc59bae824d
swobeeh <talropsteyp@gmail.com>
2026-02-03 12:11:02 +0530

    new branch

--- calculator.py
+++ calculator.py
@@ -3,20 +3,20 @@ def calculator():
     print("Operations: +  -  *  /")
 
     try:
-        num1 = float(input("Enter first number: "))
+        number1 = float(input("Enter first number: "))
         operator = input("Enter operator (+ - * /): ")
         num2 = float(input("Enter second number: "))
 
         if operator == "+":
-            result = num1 + num2
+            result = number1 + num2
         elif operator == "-":
-            result = num1 - num2
+            result = number1 - num2
         elif operator == "*":
-            result = num1 * num2
+            result = number1 * num2
         elif operator == "/":
             if num2 == 0:
                 raise ZeroDivisionError("Error: Division by zero")
-            result = num1 / num2
+            result = number1 / num2
         else:
             print("Invalid operator")
             return
